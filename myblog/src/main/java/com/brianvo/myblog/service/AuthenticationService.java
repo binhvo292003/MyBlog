a2e43d5ba437506b6bffc859aca8ad230e1e3ae9
binhvo292003 <95414345+binhvo292003@users.noreply.github.com>
2025-07-20 03:04:35 +0700

    feat: handle login for user

---
+++ myblog/src/main/java/com/brianvo/myblog/service/AuthenticationService.java
@@ -0,0 +1,9 @@
+package com.brianvo.myblog.service;
+
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface AuthenticationService {
+    UserDetails authenticate(String email, String password);
+    String generateToken(UserDetails userDetails);
+}
